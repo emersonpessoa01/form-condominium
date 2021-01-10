99ef3f024d35ce667f6a84b579483277ab415c67
emersonpessoa01 <eps011108@gmail.com>
2021-01-10 18:22:38 -0300

    updated

--- src/components/FormNew/ValidationStyled.js
+++ src/components/FormNew/ValidationStyled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const ValidationStyled = styled.h6`
-    font-size:10px;
+    font-size:10.5px;
+    padding-top: 5px
 `;
 export default ValidationStyled;
